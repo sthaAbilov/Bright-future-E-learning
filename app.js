a5c43b4ed7f962164e6f7fb1a8e1dec40b5d082f
sujanrajbharat <sujanrabharat0@gmail.com>
2021-01-06 18:33:59 +0545

    add video and file api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require ('./db/db');
 const User = require('./models/user');
-
+const Listing = require('./models/listing');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -34,7 +34,7 @@ var storage = multer.diskStorage({
 //validations
 var imageFileFilter = (req, file, cb) => {
     if (!file.originalname.match(/\.(jpg|jpeg|png|PNG|gif)$/)) {
-        return cb(new Error("You can upload only image files!"), false);
+        return cb(new Error("You can upload only video files!"), false);
     }
     cb(null, true);
 };
@@ -51,6 +51,75 @@ var upload = multer({
 app.post('/upload', upload.single('imageFile'), (req, res) => {
     res.json(req.file);
 });
+
+
+/*------------------------file upload api-------------------*/
+
+//file image
+var storage = multer.diskStorage({
+    destination: './public/files',
+    filename: (req, file, callback) => {
+        let ext = path.extname(file.originalname);
+        callback(null, file.fieldname + '-' + Date.now() + ext);
+    }
+});
+
+
+//validations
+var fileFileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(doc|docs|pdf|ppt)$/)) {
+        return cb(new Error("You can upload only  files!"), false);
+    }
+    cb(null, true);
+};
+
+var file = multer({
+    storage: storage,
+    fileFilter: fileFileFilter,
+    limits: {
+        fileSize: 1000000
+    }
+});
+
+//video 
+app.post('/file', upload.single('fileFile'), (req, res) => {
+    res.json(req.file);
+});
+
+
+/*------------------------video upload api-------------------*/
+
+//video 
+var storage = multer.diskStorage({
+    destination: './public/videos',
+    filename: (req, file, callback) => {
+        let ext = path.extname(file.originalname);
+        callback(null, file.fieldname + '-' + Date.now() + ext);
+    }
+});
+
+
+//validations
+var videoFileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/\.(mp4|webm|ogg|gif)$/)) {
+        return cb(new Error("You can upload only image files!"), false);
+    }
+    cb(null, true);
+};
+
+var video = multer({
+    storage: storage,
+    fileFilter: videoFileFilter,
+    limits: {
+        fileSize: 1000000
+    }
+});
+
+//video 
+app.post('/video', upload.single('videoFile'), (req, res) => {
+    res.json(req.file);
+});
+
 
 
 /*---------Register user API--------------*/
@@ -125,7 +194,20 @@ app.post("/login", async function (req, res) {
 });
 /*-------------------END-----Login API----END---------------------------*/
 
+/*---------------Add Listing(Add course by host user ) API----------------*/
+app.post("/addListings", auth, (req, res) => {
 
+    var listings = new Listing(req.body);
+    listings.save().then(function (listings) {
+        res.status(201).json({
+            message: "Listing Added Successfully"
+        })
+    }).catch(function () {
+        res.send(e);
+    });
+
+});
+/*-------------------END-------Add Listing API------END-------------------*/
 
 /*-------------------------------Logout User All Devices API-----------------------------*/
 app.post('/users/logoutAll', auth, async (req, res) => {
@@ -145,6 +227,20 @@ app.get('/dashboard', auth, function (req, res) {
     res.send(req.user);
 });
 /*----------------------END---- Dashboard API-----END----------------------*/
+
+
+/*--------------------Get Listings (for user) API-------------------------*/
+app.get('/getListings', function (req, res) {
+    Listing.find({
+        booking_status: false,
+        approved_status: ""
+    }).then(function (listing) {
+        res.send(listing);
+    }).catch(function (e) {
+        res.send(e);
+    });
+});
+/*--------------------END-----Get Listings API------END-------------------*/
 
 /*--------------------------------Post Comment API----------------------------------------*/
 app.post('/comment', auth, function (req, res) {
